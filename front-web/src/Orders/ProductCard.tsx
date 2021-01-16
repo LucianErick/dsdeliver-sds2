041d9dd93c998c17040fe60625c4edae32e46449
Luciano Figueiredo <luciano.erick.filho@ccc.ufcg.edu.br>
2021-01-16 14:21:06 -0300

    connection with backend using axios.

---
+++ front-web/src/Orders/ProductCard.tsx
@@ -0,0 +1,32 @@
+import {Product} from './Types';
+
+type Props = {
+    product: Product;
+}
+
+function formatPrice(price: number) {
+    const formatter = new Intl.NumberFormat('pt-br', {
+        style: 'currency',
+        currency: 'BRL',
+    });
+    return formatter.format(price);
+}
+
+function ProductCard({product}: Props) {
+    return (
+        <div className="order-card-container">
+            <h3 className="order-card-title">
+                {product.name}
+            </h3>
+            <img src={product.imageUri} alt={product.name} className="order-card-image"/> 
+            <h3 className="order-card-price">{formatPrice(product.price)}</h3>
+            <div className="order-card-description">
+                <span>Descrição</span>
+                <p>{product.description}</p>
+            </div>
+
+        </div>
+    )
+}
+
+export default ProductCard;
--- front-web/src/Orders/index.tsx
+++ front-web/src/Orders/index.tsx
@@ -1,11 +1,25 @@
+import {useEffect, useState} from 'react'
+import { fetchProducts } from '../api'
+import ProductsList from './ProductsList'
 import StepsHeader from './StepsHeader'
 import './styles.css'
+import { Product } from './Types'
 
 function Orders() {
+    
+    const [products, setProducts] = useState<Product[]>([])
+
+    useEffect(() => {
+        fetchProducts()
+        .then(response => setProducts(response.data))
+        .catch(error => console.log(error))
+    }, []);
+    
     return (
         <div className="orders-container">
             <div className="orders content">
                 <StepsHeader/>
+                <ProductsList products={products}/>
             </div>
         </div>
     )
